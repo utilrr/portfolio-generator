0e81ffafeec8a260f6eaea8c858209a0651768d7
Mark J. Healy <gmarkghealy@gmail.com>
2022-02-22 17:52:25 -0500

    igtignore &json files

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-// console.log(name, github);
-// console.log(generatePage(name, github));
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
