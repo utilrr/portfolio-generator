4f3ef3e739204569fd77bd90be1653ba55c979f5
Mark J. Healy <gmarkghealy@gmail.com>
2022-02-22 14:37:28 -0500

    fixed auto-correct in vscode

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 
@@ -48,5 +49,10 @@ const generatePage = (name, github) => {
   `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
